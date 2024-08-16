fb65ffbf906931151d2bad90b8253be6bd546932
shinyay <shinya.com@gmail.com>
2024-08-16 15:54:26 +0900

    Add sample challenge

---
+++ app/src/main/java/org/example/challenge1/HelloCopilot.java
@@ -0,0 +1,16 @@
+package org.example.challenge1;
+
+public class HelloCopilot {
+
+    HelloCopilot() {
+        System.out.println("シナリオ１：基本的なコード生成");
+    }
+
+    // メソッドの生成
+    // greeting というメソッドを生成し、その中で挨拶を出力ようにします。
+    greeting() {
+        System.out.println("Hello, Copilot!");
+    }
+
+
+}
