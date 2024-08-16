47acee3e2c714401bba57126556ac9cd3f5602e9
shinyay <shinya.com@gmail.com>
2024-08-16 07:44:12 +0000

    Add Challege 1, Code generation

--- app/src/main/java/org/example/challenge1/HelloCopilot.java
+++ app/src/main/java/org/example/challenge1/HelloCopilot.java
@@ -2,15 +2,13 @@ package org.example.challenge1;
 
 public class HelloCopilot {
 
-    HelloCopilot() {
+    public HelloCopilot() {
         System.out.println("シナリオ１：基本的なコード生成");
     }
 
     // メソッドの生成
     // greeting というメソッドを生成し、その中で挨拶を出力ようにします。
-    greeting() {
-        System.out.println("Hello, Copilot!");
-    }
+    public void greeting() {}
 
 
 }
