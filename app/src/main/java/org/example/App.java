007207f346bbe4a682b655e73438dd3ce13a90bd
shinyay <shinya.com@gmail.com>
2024-08-16 08:18:55 +0000

    Add call challenge 2

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -1,12 +1,19 @@
 package org.example;
 
 import org.example.challenge1.*;
+import org.example.challenge2.*;
+
 
 public class App {
 
 
     public static void main(String[] args) {
+        // シナリオ１：基本的なコード生成
         HelloCopilot helloCopilot = new HelloCopilot();
         helloCopilot.greeting();
+
+        // シナリオ２：Copilot チャットを使ってコード生成
+        YourIdea yourIdea = new YourIdea();
+        yourIdea.run();
     }
 }
