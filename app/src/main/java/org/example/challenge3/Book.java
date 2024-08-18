e583a17e2f3e67d8c0333bb651eed796f51e7609
shinyay <shinya.com@gmail.com>
2024-08-18 22:44:00 +0900

    Modify this project before start the challenge

--- app/src/main/java/org/example/challenge3/Book.java
+++ app/src/main/java/org/example/challenge3/Book.java
@@ -20,58 +20,6 @@ public class Book {
         this.price = price;
     }
 
-    // Setter methods
-    public void setTitle(String title) {
-        this.title = title;
-    }
 
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    public void setPublicationYear(int publicationYear) {
-        this.publicationYear = publicationYear;
-    }
-
-    public void setIsbn(String isbn) {
-        this.isbn = isbn;
-    }
-
-    public void setPrice(double price) {
-        this.price = price;
-    }
-
-    // Getter methods
-    public String getTitle() {
-        return title;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public int getPublicationYear() {
-        return publicationYear;
-    }
-
-    public String getIsbn() {
-        return isbn;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-
-    // toString method
-    @Override
-    public String toString() {
-        return "Book{" +
-                "title='" + title + '\'' +
-                ", author='" + author + '\'' +
-                ", publicationYear=" + publicationYear +
-                ", isbn='" + isbn + '\'' +
-                ", price=" + price +
-                '}';
-    }
 }
 
